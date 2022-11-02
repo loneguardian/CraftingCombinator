eeada1f01a82cd453bf6571e3106ae9e5049ad5a
Xeraph <teckpei@gmail.com>
2022-11-02 20:26:18 +0800

    Exposes whole global table for migration of signal cache.

--- crafting_combinator/script/remote-interface.lua
+++ crafting_combinator/script/remote-interface.lua
@@ -1,9 +1,6 @@
 remote.add_interface("crafting_combinator",
 {
     get_migration_data = function()
-        local migrated_state = {}
-        migrated_state.cc = global.cc
-        migrated_state.rc = global.rc
-        return migrated_state
+        return global
     end,
 })
