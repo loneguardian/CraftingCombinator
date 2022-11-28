928374c6a96e4d8111570d24703664bb7fc2c08b
Xeraph <teckpei@gmail.com>
2022-11-28 12:30:31 +0800

    `Typedef`: Fixes describe/test.each definitions.

---
+++ library/testorio.def-fixes.lua
@@ -0,0 +1,57 @@
+---@meta
+---@diagnostic disable
+
+-- describe/test.each()
+
+---@class TestCreatorBase
+local test
+
+---@generic T
+---@param values `T`[][]
+---@param name string
+---@param func fun(...:T)
+---@return TestBuilder<fun(...:T)>
+function test.each(values, name, func) end
+
+---@generic T
+---@param values `T`[]
+---@param name string
+---@param func fun(v: T)
+---@return TestBuilder<fun(v: T)>
+function test.each(values, name, func) end
+
+---@generic T
+---@param values `T`[][]
+---@return fun(name: string, func: fun(...:T))
+function test.each(values) end
+
+---@generic T
+---@param values `T`[]
+---@return fun(name: string, func: fun(v: T))
+function test.each(values) end
+
+
+---@class DescribeCreatorBase
+local describe
+
+---@generic T
+---@param values `T`[][]
+---@param name string
+---@param func fun(...:T)
+function describe.each(values, name, func) end
+
+---@generic T
+---@param values `T`[]
+---@param name string
+---@param func fun(v: T)
+function describe.each(values, name, func) end
+
+---@generic T
+---@param values `T`[][]
+---@return fun(name: string, func: fun(...:T))
+function describe.each(values) end
+
+---@generic T
+---@param values `T`[]
+---@return fun(name: string, func: fun(v: T))
+function describe.each(values) end
